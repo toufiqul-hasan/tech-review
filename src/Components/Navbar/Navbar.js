2e69f6b705c064da4f85d4c17d3cf046a028e70b
toufiqul-hasan <toufiqul.shihab@gmail.com>
2022-04-04 01:42:04 +0600

    navbar added

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="logo-container">
+        <h1>Tech Review</h1>
+      </div>
+      <div className="link-container">
+        <NavLink
+          className={({ isActive }) => (isActive ? "active-link" : "link")}
+          to="/"
+        >
+          Home
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? "active-link" : "link")}
+          to="/reviews"
+        >
+          Reviews
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? "active-link" : "link")}
+          to="/dashboard"
+        >
+          Dashboard
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? "active-link" : "link")}
+          to="/blogs"
+        >
+          Blogs
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? "active-link" : "link")}
+          to="/about"
+        >
+          About
+        </NavLink>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOMClient from "react-dom/client";
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 const container = document.getElementById("root");
@@ -8,6 +9,8 @@ const root = ReactDOMClient.createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
